e056e5155ba5d6db19f6b906520671dc6c2505d1
Securti <41710043+Securti@users.noreply.github.com>
2020-05-25 03:51:03 +0900

    Add files via upload

---
+++ Leaves.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @name Leaves
+ * @main securti\leaves\Leaves
+ * @author ["Securti"]
+ * @version 0.2
+ * @api 3.12.0
+ * @description License : LGPL 3.0
+ */
+ 
+ namespace securti\leaves;
+ 
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\block\Block;
+use pocketmine\event\block\LeavesDecayEvent;
+
+use pocketmine\level\Level;
+
+class Leaves extends PluginBase implements Listener{
+
+  public $world = ["world"];
+  
+  public static $instance;
+  
+  public static function getInstance(){
+  
+    return self::$instance;
+  }
+  public function onLoad(){
+  
+    self::$instance = $this;
+  }
+  public function onEnable(){
+  
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onLeavesDecay(LeavesDecayEvent $e){
+  
+    $block = $e->getBlock();
+    
+    $level = $block->getLevel();
+    $folder = $level->getFolderName();
+    
+    if(in_array($folder, $this->world)){
+    
+      $e->setCancelled(true);
+    }
+  }
+}
